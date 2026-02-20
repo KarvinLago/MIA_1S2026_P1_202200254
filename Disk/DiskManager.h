e165d8c3430681f0897b0d154011f093bc946429
KarvinLago <2905246950116@ingenieria.usac.edu.gt>
2026-02-20 17:42:33 -0600

    commit

--- Disk/DiskManager.h
+++ Disk/DiskManager.h
@@ -1,11 +1,3 @@
-// #pragma once
-// #include <string>
-
-// namespace DiskManager {
-//     void Mkdisk(int size, char fit, char unit, const std::string& path);
-// }
-
-
 #pragma once
 #include <string>
 

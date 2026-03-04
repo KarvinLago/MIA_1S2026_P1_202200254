db2067e22f5236aa982332f0a5cde4dc3251ac3b
KarvinLago <2905246950116@ingenieria.usac.edu.gt>
2026-03-04 17:02:57 -0600

    eliminacion de codigo viejo

--- Utilities/FileUtils.cpp
+++ Utilities/FileUtils.cpp
@@ -1,23 +1,3 @@
-// #include "FileUtils.h"
-// #include <filesystem>
-
-// namespace FileUtils {
-
-// bool CreateDiskFile(const std::string& path) {
-//     std::filesystem::create_directories(
-//         std::filesystem::path(path).parent_path()
-//     );
-
-//     std::ofstream file(path, std::ios::binary | std::ios::trunc);
-//     return file.good();
-// }
-
-// std::fstream OpenFile(const std::string& path) {
-//     return std::fstream(path, std::ios::in | std::ios::out | std::ios::binary);
-// }
-
-// }
-
 #include "FileUtils.h"
 #include <filesystem>
 
--- Utilities/FileUtils.h
+++ Utilities/FileUtils.h
@@ -1,14 +1,3 @@
-// #pragma once
-// #include <fstream>
-// #include <string>
-
-// namespace FileUtils {
-
-// bool CreateDiskFile(const std::string& path);
-// std::fstream OpenFile(const std::string& path);
-
-// }
-
 #pragma once
 #include <fstream>
 #include <string>
